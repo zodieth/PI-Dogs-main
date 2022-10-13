12e23e63dd8753cc2ae9ff8261b800cecf86d227
zodieth <contact.prodzodi@gmail.com>
2022-10-13 19:31:28 -0300

    db and back done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const axios = require("axios");
-
+const { Temper, Dog } = require("../db");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -12,36 +12,104 @@ const router = Router();
 router.get("/dogs", async (req, res) => {
   const { name } = req.query;
 
-  try {
-    if (!name) {
-      const dogs = await axios.get("https://api.thedogapi.com/v1/breeds");
+  const dogs = await axios.get("https://api.thedogapi.com/v1/breeds");
 
+  const dogsName = await axios.get(
+    `https://api.thedogapi.com/v1/breeds/search?q=${name}`
+  );
+
+  const breedsName = await dogsName.data.map((d) => {
+    return {
+      name: d.name,
+      temperament: d.temperament,
+      weight: d.weight,
+    };
+  });
+
+  try {
+    if (name) {
+      res.json(breedsName);
+    } else {
       const breeds = dogs.data.map((d) => {
         return {
           name: d.name,
-          img: d.image.url,
+          img: d.image.url ? d.image.url : "not found",
           temperament: d.temperament,
           weight: d.weight,
         };
       });
 
       res.json(breeds);
-    } else if (name) {
-      const dogsName = await axios.get(
-        `https://api.thedogapi.com/v1/breeds/search?q=${name}`
-      );
+    }
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 
-      const breedsName = dogsName.data.map((d) => {
+router.get("/dogs/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const idApi = await axios.get(`https://api.thedogapi.com/v1/breeds/${id}`);
+  const mapId = [];
+  mapId.push(idApi.data);
+
+  const data = mapId.map((e) => {
+    return {
+      name: e.name,
+      img: e.image,
+      weight: e.weight,
+      temperaments: e.temperament,
+    };
+  });
+
+  try {
+    if (id) {
+      res.json(data);
+    } else {
+      res.status(404).send("El id no existe");
+    }
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+router.get("/temperaments", async (req, res) => {
+  try {
+    const temperaments = await Temper.findAll();
+    const temperApi = await axios.get("https://api.thedogapi.com/v1/breeds");
+
+    if (!temperaments.length) {
+      const info = temperApi.data.map((e) => {
         return {
-          name: d.name,
-          img: d.image.url,
-          temperament: d.temperament,
-          weight: d.weight,
+          temperaments: e.temperament ? e.temperament : "",
         };
       });
 
-      res.json(dogsName);
+      await Temper.bulkCreate(info);
+      res.json(info);
+      ("temperaments created");
+    } else {
+      res.json(temperaments);
     }
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+router.post("/dogs", async (req, res) => {
+  const { name, height, weight, years } = req.body;
+
+  try {
+    const newDog = await Dog.findOrCreate({
+      where: {
+        name: name,
+        height: height,
+        weight: weight,
+        years: years,
+      },
+    });
+
+    res.json(newDog);
   } catch (error) {
     res.status(404).send(error);
   }
